929291d04732e6a5fd315f6b0a6f35fca3d27ed3
wannesds <wannesdieltiens@gmail.com>
2020-08-26 04:58:42 +0200

    optimizing code w new hooks, useEffect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,103 +1,145 @@
-import React, { Component } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import './App.css';
-import CardList from '../components/CardList';
-import ChangePage from '../components/ChangePage';
-import ErrorBoundry from '../components/ErrorBoundry';
-import Filter from '../components/Filter';
+import Card from '../components/Card';
+// import CardList from '../components/CardList';
+// import ChangePage from '../components/ChangePage';
+// import ErrorBoundry from '../components/ErrorBoundry';
+// import Filter from '../components/Filter';
 
-import  {quotes } from '../quotes.js';
-
-
-class App extends Component {
-  constructor() {
-    super ()
-    this.state = {
-      chars: [], 
-      info: [], 
-      fPage: '', // Future Page
-      page: 1,  // Current Page (by default 1)
-      query: '',
-      quotes: quotes
-    }
-  }
-
-  componentDidMount() {
-    fetch('https://rickandmortyapi.com/api/character/')
-      .then(response => response.json())
-      .then(character => this.setState({ chars: character.results, info: character.info}));
-
-    // fetch('../quotes.js')
-    //   .then(response => response.json())
-    //   .then(quotes => this.setState({ quotes: quotes}));
-    //  // default fetch
-  }
-
-  componentDidUpdate(prevProps, prevState) {//use useEffect!!!
-    const {fPage, page, info} = this.state;
-
-    if (prevState.fPage !== fPage) { //check for changes
-      if (fPage === info.prev) { //changes current page before new fetch
-        this.setState({page: page - 1})
-      } else {
-        this.setState({page: page + 1})
-      } //this shouldnt be needed for in react!!!
-
-      fetch(fPage)
-      .then(response => response.json())
-      .then(character => this.setState({ chars: character.results, info: character.info}));  
-    }
-
-    if (this.state.query !== '') { //gives error when useing name that does not occur in any characters
-      fetch("https://rickandmortyapi.com/api/character/" + this.state.query)
-      .then(response => response.json())
-      .then(character => this.setState({ chars: character.results, info: character.info}));
-      this.setState({query: ''})
-    }
-  }
-
-  onFilterSubmit(fullQuery) {
-    this.setState({query: fullQuery})
-  }
-
-  // onSubmit = (e) => {
-  //   e.preventDefault()
-  //   this.setState({testdata: new FormData(e.target)});
-  // }
+// import  {quotes } from '../quotes.js';
 
 
-  render() {
-    const {chars, page, info, quotes} = this.state;
+function App() {
+  const [data, setData] = useState({results: []});
+  const [query, setQuery] = useState('Rick');
 
-    console.log(quotes[1])
-    return (
-      <fragment>
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch('https://rickandmortyapi.com/api/character/?name=Rick')
+        .then(response => response.json())
+        .then(res => setData(res))
+    };
 
-      <header>
-        <div>
-          <p>{quotes[1].quote}</p>
-          <h5>- {quotes[1].name}</h5>
-        </div>
-      </header>
-      <div>
-        <Filter onSubmit={this.onFilterSubmit.bind(this)}/>
-        <ChangePage 
-          prevPage={() => this.setState({fPage: info.prev})} 
-          nextPage={() => this.setState({fPage: info.next})}
-          info={info}
+    fetchData();
+  },[])
+
+  return (
+    <Fragment>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
         />
-        <h2>page: {page}/{info.pages}</h2>
 
-        <h2>{this.state.testdata}</h2>
-
-        <ErrorBoundry>
-          <CardList chars={chars}/>
-        </ErrorBoundry> 
-      </div>
-
-      </fragment>
-    );
-  }
-
+        <div> 
+            { 
+                data.results.map((c, i) => (
+                      <Card
+                        key={i} 
+                        id={c.id} 
+                        name={c.name} 
+                        status={c.status}
+                      />
+                ))   
+            }
+        </div>
+    </Fragment>
+  )
 }
+
+
+// class App extends Component {
+//   constructor() {
+//     super ()
+//     this.state = {
+//       chars: [], 
+//       info: [], 
+//       fPage: '', // Future Page
+//       page: 1,  // Current Page (by default 1)
+//       query: '',
+//       quotes: quotes
+//     }
+//   }
+
+//   componentDidMount() {
+//     fetch('https://rickandmortyapi.com/api/character/')
+//       .then(response => response.json())
+//       .then(character => this.setState({ chars: character.results, info: character.info}));
+
+//     // fetch('../quotes.js')
+//     //   .then(response => response.json())
+//     //   .then(quotes => this.setState({ quotes: quotes}));
+//     //  // default fetch
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {//use useEffect!!!
+//     const {fPage, page, info} = this.state;
+
+//     if (prevState.fPage !== fPage) { //check for changes
+//       if (fPage === info.prev) { //changes current page before new fetch
+//         this.setState({page: page - 1})
+//       } else {
+//         this.setState({page: page + 1})
+//       }
+
+//       fetch(fPage)
+//       .then(response => response.json())
+//       .then(character => this.setState({ chars: character.results, info: character.info}));  
+//     }
+
+//     if (this.state.query !== '') { //gives error when useing name that does not occur in any characters, catcherror
+//       fetch("https://rickandmortyapi.com/api/character/" + this.state.query)
+//       .then(response => response.json())
+//       .then(character => this.setState({ chars: character.results, info: character.info}));
+//       this.setState({query: ''})
+//     }
+//   }
+
+//   // onFilterSubmit(fullQuery) {
+    
+//   // }
+
+//   // onSubmit = (e) => {
+//   //   e.preventDefault()
+//   //   this.setState({testdata: new FormData(e.target)});
+//   // }
+
+
+//   render() {
+//     const {chars, page, info, quotes, fullQuery} = this.state;
+
+//     console.log(quotes[1])
+//     return (
+//       <fragment>
+
+//       <header>
+//         <div>
+//           <p>{quotes[1].quote}</p>
+//           <h5>- {quotes[1].name}</h5>
+//         </div>
+//       </header>
+//       <div>
+//         <Filter onSubmit={() => this.setState({query: fullQuery})}/>
+//         <ChangePage 
+//           prevPage={() => this.setState({fPage: info.prev})} 
+//           nextPage={() => this.setState({fPage: info.next})}
+//           info={info}
+//         />
+//         <h2>page: {page}/{info.pages}</h2>
+
+//         <h2>{this.state.testdata}</h2>
+
+//         <ErrorBoundry>
+//         <Suspense fallback={<div>Oops Something went wrong!</div>}>
+//           <CardList chars={chars}/>
+//         </Suspense>
+//         </ErrorBoundry> 
+//       </div>
+
+//       </fragment>
+//     );
+//   }
+
+// }
 
 export default App;
