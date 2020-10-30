dd1528396336f17ae46181fbe4697b944c6e8131
wannesds <wannesdieltiens@gmail.com>
2020-10-30 20:58:10 +0100

    updated opacity cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,8 +7,8 @@ const Card = ({id, name, status, species, gender, origin, location, episode}) =>
             <img alt='Character' src={`https://rickandmortyapi.com/api/character/avatar/${id}.jpeg`}/>
             
             <div className='CardInfo'>
-                <h3>{name} <h4>- {species}</h4></h3>
-                <h5 className='Gender'>{gender}</h5>
+                <h3>{name} <h4>- {gender}</h4></h3>
+                <h5 className='Gender'>{species}</h5>
                 <p>Last location:<br></br> <h5>{location.name}</h5></p>
                 <p>Origin:<br></br> <h5>{origin.name}</h5></p>  
                 {status === 'Alive' ? null : <div className='DeadSign'></div>}
