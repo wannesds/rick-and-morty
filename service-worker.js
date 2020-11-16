a37d87f240fa27bf5b1b0e5c04b545d73dd4223a
wannesds <wannesdieltiens@gmail.com>
2020-11-16 14:45:50 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rick-and-morty/precache-manifest.4886101cf8abc9834056b75d9bbc79b7.js"
+  "/rick-and-morty/precache-manifest.259b737c6e7c8a3cf5f7e0a2b600fed8.js"
 );
 
 self.addEventListener('message', (event) => {
